4dd16e22acf99c9fa0615c49babcfc5a5753b664
Mattia Ripoli <mattia.ripoli@3tsoftwarelabs.com>
2024-09-07 10:04:53 +0200

    Update path of Python repricer.

--- Repricer.go
+++ Repricer.go
@@ -67,10 +67,10 @@ func Repricer() {
 
 	// Start python repricer if there is any book to update.
 	if len(booksToUpdate) > 0 {
-		cmd := exec.Command("/bin/bash", "/home/mattia/ebay/repricer/start_repricer.sh")
+		cmd := exec.Command("/bin/bash", "/home/mattia/repricer/start_repricer.sh")
 		output, err := cmd.CombinedOutput()
 		if err != nil {
-			_, err := fmt.Fprintf(os.Stderr, "Error executing script: %s", err)
+			_, err := fmt.Fprintf(os.Stderr, "Error in starting repricer script from GoLang to Python: %s", err)
 			if err != nil {
 				return
 			}
