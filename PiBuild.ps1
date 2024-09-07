aad3f9b841b9bf5ed9c7b88dc85bdf371b09672e
Mattia Ripoli <mattia.ripoli@3tsoftwarelabs.com>
2024-09-07 09:25:01 +0200

    Stop using sshpass as only publickey is used now.

--- PiBuild.ps1
+++ PiBuild.ps1
@@ -1,6 +1,6 @@
 $env:GOOS = "linux"
 $env:GOARCH = "arm64"
 go build -o Scraper
-wsl sshpass -p mattia scp -o StrictHostKeyChecking=no ./Scraper mattia@192.168.188.45:/home/mattia/ebay
-wsl sshpass -p mattia scp -o StrictHostKeyChecking=no ./start_scraper.sh mattia@192.168.188.45:/home/mattia/ebay
-wsl sshpass -p mattia scp -o StrictHostKeyChecking=no ./send_error.py mattia@192.168.188.45:/home/mattia/ebay
+scp -o StrictHostKeyChecking=no ./Scraper mattia@192.168.188.45:/home/mattia/ebay
+scp -o StrictHostKeyChecking=no ./start_scraper.sh mattia@192.168.188.45:/home/mattia/ebay
+scp -o StrictHostKeyChecking=no ./send_error.py mattia@192.168.188.45:/home/mattia/ebay
